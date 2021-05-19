750836db5cb2aaeef76a32df44544df50498a52c
bobmagicii <bob@majdak.net>
2021-05-19 16:24:56 -0500

    afdsafda

--- src/Nether/Composer/Plugin.php
+++ src/Nether/Composer/Plugin.php
@@ -4,7 +4,24 @@ namespace Nether\Composer;
 use Composer;
 
 class Plugin
-extends Composer\Plugin\PluginInterface {
+implements Composer\Plugin\PluginInterface {
 
+	public function
+	activate(Composer\Composer $Inst, Composer\IO\IOInterface $IO) {
+
+		return;
+	}
+
+	public function
+	deactivate(Composer\Composer $Inst, Composer\IO\IOInterface $IO) {
+
+		return;
+	}
+
+	public function
+	uninstall(Composer\Composer $Inst, Composer\IO\IOInterface $IO) {
+
+		return;
+	}
 
 }
